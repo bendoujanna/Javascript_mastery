13af6e33f6355f1d01d2d9efdd512d1b8f5c14cf
bendoujanna <b.vitalina@alustudent.com>
2026-02-02 23:25:13 +0200

    learned about js basics and variables

---
+++ Foundations/day_1.js
@@ -0,0 +1,22 @@
+const firstName = "Janna";
+let age = 20;
+let isStudent = true;
+
+console.log("Hi, my name is " + firstName + " and I am " + age + " years old. ");
+
+let language = "JavaScript";
+let num = 10;
+let isYoung = true;
+let isMarried;
+let x = null;
+console.log("");
+console.log(typeof num);
+
+// const birthYear = 2005;
+// birthYear = 2006; // This will cause an error because birthYear is a constant
+
+let score = 95;
+console.log(score);
+
+score = 100; // This is valid because score is declared with let
+console.log(score);
