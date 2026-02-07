72589ee93e03c29203296e70b9e5e27e3b861dc3
bendoujanna <b.vitalina@alustudent.com>
2026-02-07 23:00:04 +0200

    arrays

---
+++ Foundations/day5.js
@@ -0,0 +1,70 @@
+// // arrays
+
+// // grocery list
+
+// const grocery = ["milk", "bread", "eggs", "cheese"];
+
+// grocery.push("butter");
+// grocery.shift();
+
+// const newList = grocery.toSorted();
+
+// console.log(grocery);
+// console.log(newList);
+
+// // the spare array detective
+// const colors = ["red", "blue", "green", "yellow"];
+// colors[5] = "purple";
+// colors.forEach(color => {
+//     console.log(color);
+// });
+
+// const found = colors.find((val, index) => {
+//     console.log(`checking index ${index}`); 
+//     return val === undefined;
+// });
+
+// // the object shallow-copy trap
+// const users = [
+//     { name: "Alice" },
+//     { name: "Bob"}
+// ];
+
+// const usersCopy = users.slice();
+
+// usersCopy[0].name = "Charlie";
+
+// console.log(users[0].name);
+
+// array-like objects
+
+// const arrayLike = {
+//     0: "a",
+//     1: "b",
+//     length: 2
+// };
+
+// console.log(Array.isArray(arrayLike)); 
+// console.log(Array.isArray([1, 2, 3]));
+
+// Integration challenge 1: the secure data processor
+
+function createDataProcessor(...initialData) { // ... collects all arguments into an array
+    let data = Array.from(initialData);
+    return {
+        getProcessedData() {
+            return data.filter(item => item > 10)
+                          .map(item => item * 2)
+        },
+        addValue(value) {
+            data.push(value);
+            return `added ${value} to private store`;
+        }
+    };
+}
+
+const processor = createDataProcessor(5, 15, 25, 8, 12);
+processor.addValue(30);
+
+console.log(processor.getProcessedData());
+console.log(processor.data);
