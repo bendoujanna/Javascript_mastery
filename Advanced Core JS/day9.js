0a948e0efb4c85437887c205813ed05e81c5aca3
bendoujanna <b.vitalina@alustudent.com>
2026-02-12 23:58:59 +0200

    scope and hoisiting

---
+++ Advanced Core JS/day9.js
@@ -0,0 +1,41 @@
+// // scope and hoisting
+// test();
+// let user = "John";
+
+// console.log(user);
+
+// function test () {
+//     let user = "Jane";
+//     console.log(user)
+
+//     if (test) {
+//         let user = "Jack";
+//         console.log(`Inside if block: ${user}`); 
+//     }
+
+// }
+
+// test();
+
+// challenge 2
+
+// greet();
+// console.log(x);
+// console.log(y);
+// var x = 10;
+// let y = 20;
+
+// function greet() {
+//     console.log("Hello!");
+// }
+
+
+// challenge 3
+
+for (var i = 0; i <= 3; i++) {
+    console.log(i);
+}
+
+for (let j = 0; j <= 3; j++) {
+    console.log(j);
+}
