b6c5ac26de6bab50af74ff016b851694c40582df
bendoujanna <b.vitalina@alustudent.com>
2026-03-23 00:17:07 +0200

    expense tracker database

--- Professional JavaScript/day28.js
+++ Professional JavaScript/day28.js
@@ -1,1 +1,19 @@
 // expense tracker 
+let transactions = [
+    { id: 1, description: "Groceries", amount: -50, type: "expense", date: "2024-06-01" },
+    { id: 2, description: "Salary", amount: 2000, type: "income", date: "2024-06-01" },
+    { id: 3, description: "Electricity Bill", amount: -100, type: "expense", date: "2024-06-01" }
+];
+
+function saveData () {
+    JSON.stringify(transactions);
+    localStorage.setItem("transactions", JSON.stringify(transactions));
+}
+function loadData () {
+    const data = localStorage.getItem("transactions");
+    if (data) {
+        transactions = JSON.parse(data);
+    } else {
+        transactions = [];
+    }
+}
